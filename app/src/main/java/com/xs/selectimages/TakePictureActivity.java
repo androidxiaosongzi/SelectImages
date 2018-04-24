633d9cb71f1cb8e7ca4f3f1a6996e3d808a2f331
YangXiSong <yangxisong@uphicoo.com>
2018-04-24 17:49:30 +0800

    1/10

--- app/src/main/java/com/xs/selectimages/TakePictureActivity.java
+++ app/src/main/java/com/xs/selectimages/TakePictureActivity.java
@@ -2,13 +2,21 @@ package com.xs.selectimages;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2018/4/24.
  */
 
 public class TakePictureActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
 
     @Override
     protected int getLayoutId() {
@@ -22,7 +30,7 @@ public class TakePictureActivity extends BaseActivity {
 
     @Override
     protected void toolbarRightClick() {
-
+        Toast.makeText(TakePictureActivity.this,"完成",Toast.LENGTH_LONG).show();
     }
 
     @Override
